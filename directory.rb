4091ce7547a812bcac36896074a1c18723a7d87c
Toby Hamand <tobyhamand@gmail.com>
2017-06-21 16:09:07 +0200

    Completed up to Step 8:8

--- directory.rb
+++ directory.rb
@@ -1,12 +1,29 @@
+require 'date'
+
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
   students = []
-  name = gets.chomp
-  while !name.empty? do
-    students << {name: name, cohort: :november}
-    puts "Now we have #{students.count} students"
+  while true do
     name = gets.chomp
+      if name.empty?
+        break
+      end
+    puts "What cohort is #{name} in?"
+    cohort = gets.chomp.to_sym
+    cohort = Date::MONTHNAMES[Date.today.month].to_sym if cohort.empty? #assign current month if no answer given
+    puts "Where is #{name} from?"
+    cob = gets.chomp
+    puts "What is #{name}'s height?"
+    height = gets.chomp
+    heightnum = height.to_i
+      while height.is_a?(Integer) == false && heightnum == 0
+        puts "Please enter a valid height"
+        height = gets.chomp
+        heightnum = height.to_i
+      end
+    students << {name: name.capitalize, cohort: cohort, country: cob.capitalize, height: height}
+    puts "Now we have #{students.count} students. Enter next student"
   end
   students
 end
@@ -16,9 +33,19 @@ def print_header
   puts "----------"
 end
 
+#def print(students)
+#  students.each_with_index do |student, index|
+#    if (student[:name].downcase.start_with? ("m")) && (student[:name].length > 12)
+#      puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)"
+#    end
+#  end
+#end
+
 def print(students)
-  students.each do |student|
-    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+  index = students.count - 1
+  while index >= 0
+    puts "#{index + 1}. #{students[index][:name].ljust(20)} (#{students[index][:cohort]} cohort)"
+    index = index - 1
   end
 end
 
